3bfb30865167ff14b11b865495cf3770e4dd4577
Vineeth Pokkavayalil <41360403+vineeth-pgmr@users.noreply.github.com>
2024-01-20 10:49:46 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+/* 1. Printing in C++:
+Question 1.1: Write a C++ code snippet using the cout statement to print
+the message “Welcome to C++ programming!” on the console.*/
+
+
+#include<iostream>
+using namespace std;
+int main() {
+   cout << "Hello World. This is C++ program" << endl;
+   return 0;
+}
